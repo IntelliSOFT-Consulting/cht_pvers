3b2c1982c38eccb09179e0ab318621037c2f7552
Itskiprotich <keeprawteachjapheth@gmail.com>
2023-06-26 09:48:27 +0000

    (core) roles update

--- setup.sh
+++ setup.sh
@@ -4,14 +4,14 @@ if [ "$1" == "up" ]; then
   # start up docker containers
   CHT_COMPOSE_PROJECT_NAME=app-devl COUCHDB_SECRET=foo DOCKER_CONFIG_PATH=${PWD} COUCHDB_DATA=${PWD}/couchd CHT_COMPOSE_PATH=${PWD} COUCHDB_USER=medic COUCHDB_PASSWORD=password docker compose -p cht_pvers_hie -f ./hie/docker-compose.cht-core.yml -f ./hie/docker-compose.cht-couchdb.yml -f ./hie/docker-compose.yml up -d --build
 elif [ "$1" == "dev" ]; then
-  # start up docker containers
-  NGINX_HTTP_PORT=8081 NGINX_HTTPS_PORT=8444 CHT_COMPOSE_PROJECT_NAME=app-devl COUCHDB_SECRET=foo DOCKER_CONFIG_PATH=${PWD} COUCHDB_DATA=${PWD}/couchd CHT_COMPOSE_PATH=${PWD} COUCHDB_USER=medic COUCHDB_PASSWORD=password docker compose -p cht_pvers_hie -f ./hie/docker-compose.cht-core.yml -f ./hie/docker-compose.cht-couchdb.yml -f ./hie/docker-compose-dev.yml up -d --build
+  # start up docker containers 
+  CHT_COMPOSE_PROJECT_NAME=app-devl COUCHDB_SECRET=foo DOCKER_CONFIG_PATH=${PWD} COUCHDB_DATA=${PWD}/couchd CHT_COMPOSE_PATH=${PWD} COUCHDB_USER=medic COUCHDB_PASSWORD=password docker compose -p cht_pvers_hie -f ./hie/docker-compose.cht-core.yml -f ./hie/docker-compose.cht-couchdb.yml -f ./hie/docker-compose-dev.yml up -d --build
 elif [ "$1" == "publish" ]; then
-  cd cht_app && cht --url=https://medic:password@localhost:8444 --accept-self-signed-certs compile-app-settings upload-app-settings && cht --url=https://medic:password@localhost:8444 --accept-self-signed-certs
+  cd cht_app && cht --url=https://medic:password@localhost --accept-self-signed-certs compile-app-settings upload-app-settings && cht --url=https://medic:password@localhost --accept-self-signed-certs
 elif [ "$1" == "down" ]; then
   docker compose -p cht_pvers_hie -f ./hie/docker-compose.yml -f ./hie/docker-compose-dev.yml -f ./hie/docker-compose.cht-core.yml -f ./hie/docker-compose.cht-couchdb.yml down
 elif [ "$1" == "setup-creds" ]; then
-  curl -X PUT -H "Content-Type: text/plain" https://medic:password@localhost:8444/api/v1/credentials/openhim -d 'password' --insecure
+  curl -X PUT -H "Content-Type: text/plain" https://medic:password@localhost/api/v1/credentials/openhim -d 'password' --insecure
 elif [ "$1" == "logs" ]; then
   docker compose -p cht_pvers_hie -f ./hie/docker-compose.yml -f ./hie/docker-compose-dev.yml -f ./hie/docker-compose.cht-core.yml -f ./hie/docker-compose.cht-couchdb.yml logs $2 -f
 else
