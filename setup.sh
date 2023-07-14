dbdbc8d8b4cdf68b890991784ce82eace15d93a4
Itskiprotich <keeprawteachjapheth@gmail.com>
2023-07-14 01:54:49 +0000

    sync

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 if [ "$1" == "up" ]; then
   # start up docker containers
-  # CHT_COMPOSE_PROJECT_NAME=app-devl COUCHDB_SECRET=foo DOCKER_CONFIG_PATH=${PWD} COUCHDB_DATA=${PWD}/couchd CHT_COMPOSE_PATH=${PWD} COUCHDB_USER=medic COUCHDB_PASSWORD=password docker compose -p cht_pvers_hie -f ./hie/docker-compose.cht-core.yml -f ./hie/docker-compose.cht-couchdb.yml -f ./hie/docker-compose.yml up -d --build
-  docker compose -p cht_pvers_hie -f ./hie/docker-compose.cht-core.yml -f ./hie/docker-compose.cht-couchdb.yml -f ./hie/docker-compose.yml up -d --build
+  CHT_COMPOSE_PROJECT_NAME=app-devl COUCHDB_SECRET=foo DOCKER_CONFIG_PATH=${PWD} COUCHDB_DATA=${PWD}/couchd CHT_COMPOSE_PATH=${PWD} COUCHDB_USER=medic COUCHDB_PASSWORD=password docker compose -p cht_pvers_hie -f ./hie/docker-compose.cht-core.yml -f ./hie/docker-compose.cht-couchdb.yml -f ./hie/docker-compose.yml up -d --build
+  # docker compose -p cht_pvers_hie -f ./hie/docker-compose.cht-core.yml -f ./hie/docker-compose.cht-couchdb.yml -f ./hie/docker-compose.yml up -d --build
 
 elif [ "$1" == "dev" ]; then
   # start up docker containers 
