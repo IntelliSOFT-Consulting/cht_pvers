12ca5910a0f2fd15b26153c61287423adc0ecfe8
itskiprotich <keeprawteachjapheth@gmail.com>
2023-07-20 15:46:02 +0300

    death marking

--- cht_app/contact-summary.templated.js
+++ cht_app/contact-summary.templated.js
@@ -16,7 +16,7 @@ const getNewestReport = (reports = []) => {
 
 
 
-const fields = [ 
+const fields = [
   { appliesToType: 'person', label: 'contact.age', value: thisContact.date_of_birth, width: 4, filter: 'age' },
   { appliesToType: 'person', label: 'contact.sex', value: 'contact.sex.' + thisContact.sex, translate: true, width: 4 },
   { appliesToType: 'person', label: 'person.field.phone', value: thisContact.phone, width: 4 },
@@ -48,6 +48,20 @@ const cards = [
         width: 6,
       }
     ]
+  },
+  {
+    label: 'contact.profile.death.title',
+    appliesToType: 'person',
+    appliesIf: () => contact && contact.date_of_date,
+    fields: () => ([
+      {
+        label: 'contact.profile.death.date',
+        value: contact.date_of_death,
+        filter: 'simpleDate',
+        translate: false,
+        width: 6
+      }
+    ]),
   }
 ];
 
