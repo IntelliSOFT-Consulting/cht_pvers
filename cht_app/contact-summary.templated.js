2e0bf9f935fa1c11778c86b17fc2befca259cf6d
itskiprotich <keeprawteachjapheth@gmail.com>
2023-07-20 16:06:58 +0300

    death confirmation

--- cht_app/contact-summary.templated.js
+++ cht_app/contact-summary.templated.js
@@ -1,20 +1,6 @@
 const thisContact = contact;
 const thisLineage = lineage;
-const allReports = reports;
-
-const assessmentForms = ['padr'];
-
-const getNewestReport = (reports = []) => {
-  let results = null;
-  reports.forEach(function (reports) {
-    if (!results || reports.reported_date > results.reported_date) {
-      results = reports;
-    }
-  });
-  return results;
-};
-
-
+ 
 
 const fields = [
   { appliesToType: 'person', label: 'contact.age', value: thisContact.date_of_birth, width: 4, filter: 'age' },
@@ -24,31 +10,6 @@ const fields = [
 ];
 
 const cards = [
-  {
-    label: 'contact.profile.assessment_history',
-    appliesToType: 'report',
-    appliesIf: (report) => {
-      const assessmentForm = getNewestReport(allReports, assessmentForms);
-      return assessmentForm.reported_date >= report.reported_date;
-    },
-    fields: [
-      {
-        label: 'contact.profile.most_recent_assessment.date',
-        value: (report) => {
-          return report.reported_date;
-        },
-        filter: 'simpleDate',
-        width: 6
-      },
-      {
-        label: 'contact.profile.type',
-        value: (report) => {
-          return report.fields.type;
-        },
-        width: 6,
-      }
-    ]
-  },
   {
     label: 'contact.profile.death.title',
     appliesToType: 'person',
